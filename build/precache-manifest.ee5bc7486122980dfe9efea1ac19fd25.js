7eb4a5a805d572ae4544bbe76fbc719d819d1615
aaldyy <aldirenaldialamin@Gmail.com>
2022-07-08 10:19:48 +0800

    fix8juli

---
+++ build/precache-manifest.ee5bc7486122980dfe9efea1ac19fd25.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f971de3f5f2a075afb5831a19ee05ea",
+    "url": "/alzamecookies/index.html"
+  },
+  {
+    "revision": "9685d1f4e77d655fb9ba",
+    "url": "/alzamecookies/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "4937107584ed81154911",
+    "url": "/alzamecookies/static/css/main.4256a972.chunk.css"
+  },
+  {
+    "revision": "9685d1f4e77d655fb9ba",
+    "url": "/alzamecookies/static/js/2.b6becb44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alzamecookies/static/js/2.b6becb44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4937107584ed81154911",
+    "url": "/alzamecookies/static/js/main.563cf10a.chunk.js"
+  },
+  {
+    "revision": "d4503f577b177cee6d55",
+    "url": "/alzamecookies/static/js/runtime-main.d65a9157.js"
+  },
+  {
+    "revision": "a6b6ff3df7a7442a7e3382c4116baa83",
+    "url": "/alzamecookies/static/media/Renaldi2.a6b6ff3d.jpg"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/alzamecookies/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/alzamecookies/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "ab983325494ece6be0367fb9d5fc555d",
+    "url": "/alzamecookies/static/media/logo1ww.ab983325.svg"
+  },
+  {
+    "revision": "38a487a23ffd4cf70b20cac3cc6b1dbf",
+    "url": "/alzamecookies/static/media/megawati.38a487a2.jpg"
+  },
+  {
+    "revision": "f7a6e18069e6db5be5c95a70bf096a5c",
+    "url": "/alzamecookies/static/media/megawati1.f7a6e180.jpg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/alzamecookies/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/alzamecookies/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "901a3a43bfc8c645bcb12b32cced5ece",
+    "url": "/alzamecookies/static/media/preview1.901a3a43.jpg"
+  },
+  {
+    "revision": "a9c56a6edd5f6aeae95fd4b42043a55a",
+    "url": "/alzamecookies/static/media/preview2.a9c56a6e.jpg"
+  },
+  {
+    "revision": "be72266af53301c826cb48418a42d4be",
+    "url": "/alzamecookies/static/media/preview3.be72266a.png"
+  },
+  {
+    "revision": "64aa6131e2082b900d61843b3d0e5823",
+    "url": "/alzamecookies/static/media/preview4.64aa6131.png"
+  },
+  {
+    "revision": "981f98ec1f298846fdf5a1bf6fab1e5b",
+    "url": "/alzamecookies/static/media/preview5.981f98ec.png"
+  },
+  {
+    "revision": "3941becca1f32af34822c0b1559d0031",
+    "url": "/alzamecookies/static/media/preview6.3941becc.jpg"
+  },
+  {
+    "revision": "b41e59d361d19289e819c5de48d79e90",
+    "url": "/alzamecookies/static/media/sss.b41e59d3.jpg"
+  }
+]);
